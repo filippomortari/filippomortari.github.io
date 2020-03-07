a78fa2485ff632eafea329bf495d4baf9fe1fba7
Filippo Mortari <filippo.mortari@gmail.com>
2020-03-07 19:29:17 +0000

    formatting

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,60 +1,60 @@
 
 var sections = [
-		 " a passionate software engineer based in London",
-		
-		 " a hard worker and a voracious learner",
-		
-		 " watching tests fail, making them pass, then refactoring",
-		
-		 " an independent thinker",
-		
-		 " somewhere in London complaining about life in London",
-		
-		 " making mistakes",
-		
-		 " fixing the letter-spacing of this title",
-		
-		 " squinting at that tedious stack trace",
-
-		 " automating everything. Hail to DevOps!",
-
-		 " still convinced he could be part of change",
-		
-		 " finally updating his CV",
-		
-		 " thinking before acting",
-		
-		 " working with people, not for people",
-		
-		 " fighting for beauty",
-		
-		 " dancing to Joy Division",
-		
-		 " is sad unless he's doing good work",
-		
-		 " trying to understand the end of Inception",
-		
-		 " working on top secret side projects",
-		
-		 " drinking a beer in Peckham",
-		
-		 " gigging with his band around London",
-		
-		 " not great at sports but reasonably good at designing software",
-		
-		 " a serious night owl",
-		
-		 " trying to make you smile",
-		
-		 " questioning the added value of his last git commit message",
-		
-		 " curious to know you",
-		
-		 " missing the morning alarm again",
-		
-		 " looking forward to his next travel adventure"
-		
-	];
+	" a passionate software engineer based in London",
+	
+	" a hard worker and a voracious learner",
+	
+	" watching tests fail, making them pass, then refactoring",
+	
+	" an independent thinker",
+	
+	" somewhere in London complaining about life in London",
+	
+	" making mistakes",
+	
+	" fixing the letter-spacing of this title",
+	
+	" squinting at that tedious stack trace",
+	
+	" automating everything. Hail to DevOps!",
+	
+	" still convinced he could be part of change",
+	
+	" finally updating his CV",
+	
+	" thinking before acting",
+	
+	" working with people, not for people",
+	
+	" fighting for beauty",
+	
+	" dancing to Joy Division",
+	
+	" is sad unless he's doing good work",
+	
+	" trying to understand the end of Inception",
+	
+	" working on top secret side projects",
+	
+	" drinking a beer in Peckham",
+	
+	" gigging with his band around London",
+	
+	" not great at sports but reasonably good at designing software",
+	
+	" a serious night owl",
+	
+	" trying to make you smile",
+	
+	" questioning the added value of his last git commit message",
+	
+	" curious to know you",
+	
+	" missing the morning alarm again",
+	
+	" looking forward to his next travel adventure"
+	
+];
 
 
 var options = {
@@ -65,21 +65,3 @@ var options = {
 };
 
 var typed = new Typed('.jstext', options);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
